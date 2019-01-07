01735acb706953753407fee2d7e6386047fc226e
Petri Alajoki <mordex@gmail.com>
2019-01-08 01:09:43 +0200

    1

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Petri Alajoki' });
+  res.render('about', { title: 'Petri Alajoki' });
 });
 
 module.exports = router;

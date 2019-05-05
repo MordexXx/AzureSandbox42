ef60ecf134f70324170cf9f207aed0bcaf4b02ec
Petri Alajoki <mordex@gmail.com>
2019-05-06 02:48:14 +0300

    1

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -18,6 +18,7 @@ let URL = window.location.href;
 // Set Initial State Of Menu
 let showMenu = false;
 
+/*
 if(URL.indexOf('about')) {
     showMenu = true;
 }
@@ -27,9 +28,23 @@ else if(URL.indexOf('portfolio')) {
 else {
     showmenu = false;
 }
+*/
 
 document.addEventListener('readystatechange', event => {
+    if (event.target.readyState === "complete") {
+        menuBtn.classList.remove('close');
+        menuBtn.classList.add('blink');
+        header1.classList.remove('show');
+        menu.classList.remove('show');
+        portrait.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+        //Set Menu State
+        showMenu = false;
+    }
 
+    /*
     if (event.target.readyState === "complete" && URL.indexOf("about")>=0) {
         menuBtn.classList.remove('close');
         menuBtn.classList.add('blink');
@@ -68,7 +83,7 @@ document.addEventListener('readystatechange', event => {
     }
     else {
         showMenu = false;
-    }
+    }*/
 
 });
 

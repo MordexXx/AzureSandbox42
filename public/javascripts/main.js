44281e493da7f7466c6935bc31ea29859b2aea32
Petri Alajoki <mordex@gmail.com>
2019-05-06 02:10:36 +0300

    1

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,26 +6,18 @@ const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const header1 = document.querySelector('.lg-heading');
 const navItems = document.querySelectorAll('.nav-item');
+const navHome = document.querySelector('.home');
+const navAbout = document.querySelector('.about');
+const navPortfolio = document.querySelector('.portfolio');
+const navContact = document.querySelector('.contact');
+
 
 
 
 // Set Initial State Of Menu
 let showMenu = false;
-navItems.addEventListener('click',function(){
-    menuBtn.classList.remove('close');
-    menuBtn.classList.add('blink');
-    header1.classList.remove('show');
-    menu.classList.remove('show');
-    portrait.classList.remove('show');
-    menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
-    navItems.forEach(item => item.classList.remove('show'));
-    
-    //Set Menu State
-    showMenu = false;
-   }, false);
 
-
+navAbout.addEventListener('click', toggleMenu);
 
 menuBtn.addEventListener('click', toggleMenu);
 

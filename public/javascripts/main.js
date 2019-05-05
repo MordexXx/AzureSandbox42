1d7c9abafa608843d139ce3d46b30f413d7ce3fd
Petri Alajoki <mordex@gmail.com>
2019-05-06 01:54:19 +0300

    1

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,16 +11,29 @@ const navItems = document.querySelectorAll('.nav-item');
 
 // Set Initial State Of Menu
 let showMenu = false;
-
+setTimeout(function(){
+    menuBtn.classList.remove('close');
+    menuBtn.classList.add('blink');
+    header1.classList.remove('show');
+    menu.classList.remove('show');
+    portrait.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+    
+    //Set Menu State
+    showMenu = false;
+   }, 2000);
+  /*
 document.addEventListener('DOMContentLoaded', function() {
-    /*
+  
     let URL=document.URL;
     console.log(URL);
     if(URL == 'http://www.webdeveloper.fi/about') {
         showMenu = false;
         toggleMenu();
     }
-    */
+   
    menuBtn.classList.remove('close');
    menuBtn.classList.add('blink');
    header1.classList.remove('show');
@@ -34,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function() {
    showMenu = false;
 
  }, false);
- 
+  */
 
 menuBtn.addEventListener('click', toggleMenu);
 
